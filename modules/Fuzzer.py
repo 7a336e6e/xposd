1b670abb89f2840337032f308e595fbf447f34ee
z3nn <7a336e6e@protonmail.com>
2021-01-01 02:06:04 +0200

    working on dfuz

--- modules/Fuzzer.py
+++ modules/Fuzzer.py
@@ -1,4 +1,7 @@
 import os.path
+import time
+import threading
+import concurrent.futures
 
 class Directory:
     def __init__(self):
@@ -6,14 +9,36 @@ class Directory:
         self.ports = None
         self.wordlist = '/usr/share/wordlists/dirbuster/directory-list-2.3-medium.txt'
 
+        self.workers = 1000
+
     def set_target(self, ip):
         self.target = ip
     
     def set_ports(self, ports):
         self.ports = ports
+
+    def set_workers(self, val):
+        self.workers = val
     
     def set_wordlist(self, wl_path):
         if path.exists(wl_path):
             self.wordlist = wl_path
         else:
             print(f'[!] Provided wordlist path ({wl_path}) does not exist on current system, fallback to default wl({self.wordlist})')
+
+    def get_request(self, path):
+        dir_path = path.replace("\n","")
+        # ToDo: Implement requests for directory
+
+    def run_dfuz(self):
+        wl = open(self.wordlist, 'r').readlines()
+
+        start = time.perf_counter()
+        print('[-] Starting directory fuzzing')
+
+        with concurrent.futures.ThreadPoolExecutor(max_workers=self.workers) as executor:
+            _ = executor.map(self.get_request, wl)
+
+        finish = time.perf_counter()
+
+        print(f'[-] Finised directory fuzzing')
--- xposd.py
+++ xposd.py
@@ -11,6 +11,8 @@ parser = argparse.ArgumentParser(description='Process arguments for script')
 parser.add_argument('--target', '-t', metavar='', help='(required) Target IP or Hostname (wihout http://)', required=True)
 parser.add_argument('--latency', '-l', metavar='', type=float, help='(optional) Time to wait for Socket connection. Default value 0.5 seconds', required=False)
 parser.add_argument('--workers', '-w', metavar='', type=int, help='(optional) Number of concurrent workers. Default value 1000', required=False)
+parser.add_argument('--wordlist', '-wl', metavar='', type=int, help='(optional) Wordlist to use for directory fuzzing', required=False)
+parser.add_argument('--extensions', '-x', metavar='', type=int, help='(optional) Set extensions for directory/path fuzzing', required=False)
 # this argument is not used at anything yet ... I have some ideas
 parser.add_argument('--port', '-p', metavar='', type=int, help='(optional) Use this value to start a scan for a single port', required=False)
 args =parser.parse_args()
@@ -38,6 +40,10 @@ def main(args):
     nmap.run_scan()
 
     http_services = nmap.get_http_services()
+    dfuz = dfuzz()
+    dfuz.set_ports(http_services)
+    dfuz.set_target(args.target)
+    dfuz.run_dfuz()
 
 if __name__ == '__main__':
     main(args)
